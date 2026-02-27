4806e3eb554be6fbba4e9bdcc8bde21465fc173b
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2026-02-27 17:18:02 +0200

    defined data types + created initial vm constructor

--- inc/PaintedRealityVM.h
+++ inc/PaintedRealityVM.h
@@ -5,7 +5,86 @@
 
 #define _GNU_SOURCE
 
-#include <spobutils.h>
+#include <spobutils/spobutils.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define ErrMem (0x01)
+
+/*
+    PaintedReality VM
+        16-bit CPU
+            AX
+            BX
+            CX
+            DX
+            SP
+            IP (PC)
+        65 KB memory -> 65536 possible memory addresses
+        (Serial COM Port)
+        (Floppy Disk Drive)
+*/
+
+typedef unsigned short int Reg;
+
+struct s_registers
+{
+    Reg ax;
+    Reg bx;
+    Reg cx;
+    Reg dx;
+    Reg sp;
+    Reg ip;
+};
+typedef struct s_registers Registers;
+
+struct s_cpu
+{
+    Registers r;
+};
+typedef struct s_cpu CPU;
+
+typedef int8 Stack[((unsigned int)(-1))]; // sequence/array or bytes
+
+enum e_opcode
+{
+    mov = 0x01,
+    nop = 0x02,
+};
+typedef enum e_opcode Opcode;
+
+struct s_instrmap
+{
+    Opcode o;
+    int8 size;
+};
+typedef struct s_instrmap IM;
+
+typedef int8 Args;
+
+struct s_instruction
+{
+    Opcode o;
+    Args a[]; // 0-2 bytes args
+};
+typedef struct s_instruction Instruction;
+
+typedef Instruction Program; // array/sequence of instructions
+
+struct s_vm
+{
+    CPU c;
+    Stack s;
+    Program *p;
+};
+typedef struct s_vm VM;
+
+static IM instrmap[] = {
+    {mov, 0x03},
+    {nop, 0x01}
+};
+
+VM *virtualmachine(Program* prog, int16 progsz);
 
 #endif
-
--- src/PaintedRealityVM.c
+++ src/PaintedRealityVM.c
@@ -1,3 +1,43 @@
 /* PaintedRealityVM.c */
 #include "PaintedRealityVM.h"
 
+VM *
+virtualmachine(Program* prog, int16 progsz)
+{
+    VM *p;
+    Program *pp;
+    int16 size;
+
+    assert((prog) && (progsz));
+
+    size = sizeof(struct s_vm);
+    p = (VM *)malloc($i size);
+    if (!p)
+    {
+        errno = ErrMem;
+        goto vm_alloc_err;
+    }
+
+    // initialize all fields in p to 0
+    zero($1 p, size);
+
+    pp = (Program *)malloc($i progsz);
+    if (!pp)
+    {
+        errno = ErrMem;
+        goto p_alloc_err;
+    }
+
+    memcpy(pp, prog, progsz);
+
+    // should initialize vm's program with pp
+
+    goto out;
+
+p_alloc_err:
+    free(p);
+vm_alloc_err:
+    p = (VM *)0;
+out:
+    return p;
+}
